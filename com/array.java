22d2efdd4daa90200aa7c48199b52c3ec6ae90ee
Yashshakya1 <160310180+Yashshakya1@users.noreply.github.com>
2024-02-24 15:45:08 +0530

    ok

--- com/array.java
+++ com/array.java
@@ -48,6 +48,53 @@ public class array  {
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
+            // 2nd example of array in for loop //
+
+            System.out.println("the success keywards ");
+            String[] dream = {"passinate","determenation","dadication","hard work","never give up"};
+            for(int d = 0; d<dream.length; d++){
+                   System.out.println(dream[d]);
+            }
+
+        System.out.println("this is a key of success to achieve your not a dream it's aim your life ");
+
+            // in reverse order array in loop //
+
+          String[] sweets ={"1.cake","2.choclate","3.Ladoo"};
+          for(int a = sweets.length -1; a>=0; a-- ){
+              System.out.println(sweets[a]);
+          }
+
+           // each loop in java for displaying the array //
+
+          for(String element:sweets){
+              System.out.println(element);
+          }
+
+          /*
+            this loop is usefull when you need only array element.
+             whenever you used the array travrsal.
+
+             { array travrsal used when
+             you need the element of the array
+              and value of i }
+
+            */
+
+            // this is a question to more strong commend in the all types of array //
+
+           // q1 Write a Java program to sum values of an array.
+
+           // Declare and initialize a variable to store the sum.
+            int sum =0;
+           // Iterate over each element of the array using an enhanced for loop.
+
+            int[] arraysum ={1,2,3,4,5,6,7,8,9,10};
+            for(int op: arraysum ){
+               sum+=op;
+            }
+              System.out.println("the sum of the array is " + sum );
+
 
     }
 }
